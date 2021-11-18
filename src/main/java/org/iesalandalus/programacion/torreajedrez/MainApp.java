13b9d3a92b434098c7257f621150fbf21e8e0d68
profi <profi@DESKTOP-695H3QE>
2021-11-18 11:20:03 +0100

    Creamos el método elegirColor en Main

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class MainApp {
 
 	private static Torre torre;
@@ -19,11 +21,36 @@ public class MainApp {
 	private static void mostrarMenu() {
 		System.out.println("Menú");
 		System.out.println("----------------------------");
-		System.out.println("1. Crear torre por defecto");
-		System.out.println("2. Crear torre de un color");
-		System.out.println("3. Crear torre con columna inicial");
-		System.out.println("4. Mover la torre");
-		System.out.println("5. Salir");
+		System.out.println("1- Crear torre por defecto");
+		System.out.println("2- Crear torre de un color");
+		System.out.println("3- Crear torre con columna inicial");
+		System.out.println("4- Mover la torre");
+		System.out.println("5- Salir");
+	}
+	
+	private static int elegirOpcion() {
+		int eleccion = 0;
+		do {
+		System.out.println("Por favor, elija una opción de entre las anteriores");
+		eleccion=Entrada.entero();
+		} while (eleccion<1 || eleccion>5);
+		return eleccion;
+	}
+	
+	private static Color elegirColor() {
+		int eleccionColor = 0;
+		do {
+		System.out.println("Por favor, elija un color para su torre:");
+		System.out.println("1- Negra");
+		System.out.println("2- Blanca");
+		eleccionColor=Entrada.entero();
+		} while (eleccionColor!=1 && eleccionColor!=2);
+		if (eleccionColor==1) {
+			return Color.NEGRO;
+		}
+		else {
+			return Color.BLANCO;
+		}
 	}
 	
 	
