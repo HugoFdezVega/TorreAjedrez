73f8af76bedb6e1bb7285dd0b5b4c584beb28d36
profi <profi@DESKTOP-695H3QE>
2021-11-18 11:02:46 +0100

    Creamos el método mosrarMenu en Main

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -16,4 +16,33 @@ public class MainApp {
 		
 	}
 	
+	private static void mostrarMenu() {
+		System.out.println("Menú");
+		System.out.println("----------------------------");
+		System.out.println("1. Crear torre por defecto");
+		System.out.println("2. Crear torre de un color");
+		System.out.println("3. Crear torre con columna inicial");
+		System.out.println("4. Mover la torre");
+		System.out.println("5. Salir");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
